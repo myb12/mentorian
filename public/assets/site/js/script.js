d0c1a28f0bbc21d1c970a6e59c8daf3b073fd76e
myb12 <mohammadyasinbappy@outlook.com>
2021-08-14 03:15:33 +0600

    Active color function improved

--- public/assets/site/js/script.js
+++ public/assets/site/js/script.js
@@ -1,8 +1,41 @@
 /*==================== LINK ACTIVE ====================*/
+/* using jQuery  */
+// function activeColor(className) {
+//     const links = $(`.${className}`);
+//     $(document).ready(function() {
+//         for (let link of links) {
+//             if (link.href && link.href == window.location) {
+//                 link.className += ` active`;
+//                 return;
+//             } else if (
+//                 !link.href &&
+//                 link.closest("a").href == window.location
+//             ) {
+//                 link.className += ` active`;
+//                 return;
+//             }
+//         }
+
+//     });
+// }
+
+function ready(callback) {
+    // in case the document is already rendered
+    if (document.readyState != "loading") callback();
+    // modern browsers
+    else if (document.addEventListener)
+        document.addEventListener("DOMContentLoaded", callback);
+    // IE <= 8
+    else
+        document.attachEvent("onreadystatechange", function() {
+            if (document.readyState == "complete") callback();
+        });
+}
+
 function activeColor(className) {
-    const links = $(`.${className}`);
-    $(document).ready(function() {
-        for (let link of links) {
+    const links = document.querySelectorAll(`.${className}`);
+    ready(function() {
+        links.forEach(link => {
             if (link.href && link.href == window.location) {
                 link.className += ` active`;
                 return;
@@ -13,8 +46,7 @@ function activeColor(className) {
                 link.className += ` active`;
                 return;
             }
-        }
-        
+        });
     });
 }
 
